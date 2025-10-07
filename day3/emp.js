0df03d08a6b8a31108c905c3965de28db8855e3c
Tharun123-Dev <anjali.tharun9949@gmail.com>
2025-10-07 12:01:59 +0530

    nani

---
+++ day3/emp.js
@@ -0,0 +1,33 @@
+employee_name=prompt("enter a employee name: ");
+emp_id=prompt("enter a employee id: ");
+age=prompt("enter age: ");
+email=prompt("enter email: ");
+salary=prompt("enter salary: ");
+date_of_joined=prompt("enter year and month of join: ");
+job=prompt("enter emp role: ");
+loc=prompt("enter loc: ");
+phno=prompt("enter phno: ");
+
+
+document.write("employee details:<br>");
+document.write(employee_name,"<br>");
+document.write(emp_id,"<br>");
+document.write(age,"<br>");
+document.write(email,"<br>");
+document.write(salary,"<br>");
+document.write(date_of_joined,"<br>");
+document.write(job,"<br>");
+document.write(loc,"<br>");
+document.write(phno);
+
+
+console.log(employee_name);
+console.log(emp_id);
+console.log(age);
+console.log(email);
+console.log(salary);
+console.log(date_of_joined);
+console.log(phno);
+console.log(job);
+console.log(location);
+console.log(phno);
