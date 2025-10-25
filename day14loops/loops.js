fd5fbb756954e6fc8a80fec597f4388873a9eeb1
Tharun123-Dev <anjali.tharun9949@gmail.com>
2025-10-25 20:01:26 +0530

    lpps

---
+++ day14loops/loops.js
@@ -0,0 +1,21 @@
+// //for loop
+// for (i=0;i<=5;i++){
+//     console.log(i)
+// }
+
+// for (i=5;i>=0;i--){
+//     console.log(i)
+// }
+// //while loop
+// var i =0;
+// while(i<=5){
+//     console.log(i);
+//     i++;
+// }
+
+//while loop
+var i =2;
+while(i<=10){
+    console.log(i);
+    i+=2;
+}
